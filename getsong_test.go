3b13905cbb10af593120244942d6e20421ef38d3
Zack Scholl <zack.scholl@gmail.com>
2019-01-12 08:02:29 -0800

    add travis ci

--- getsong_test.go
+++ getsong_test.go
@@ -1,6 +1,7 @@
 package getsong
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,8 +12,9 @@ func TestGetSong(t *testing.T) {
 
 	id, err := GetMusicVideoIDs("transmission listen helado negro", 224)
 	assert.Nil(t, err)
-	assert.Equal(t, "d", id)
+	assert.Equal(t, "JkIM2xp65B8", id)
 	fname, err := DownloadYouTube("JkIM2xp65B8", "Helado Negro - Transmission")
 	assert.Nil(t, err)
 	assert.Nil(t, ConvertToMp3(fname))
+	os.Remove(fname)
 }
