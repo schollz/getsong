ec00a0b7ec33c257a161dfba7d707541f549624c
Zack Scholl <zack.scholl@gmail.com>
2019-01-14 20:07:52 -0800

    fix example

--- cmd/getsong/main.go
+++ cmd/getsong/main.go
@@ -8,8 +8,17 @@ import (
 )
 
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Println(`Usage:
+		
+getsong 'title of song' 'artist of song'
+
+`)
+		os.Exit(1)
+	}
 	fname, err := getsong.GetSong(getsong.Options{
 		Title:        os.Args[1],
+		Artist:       os.Args[2],
 		ShowProgress: true,
 		Debug:        true,
 	})
